63bffc51f2843938d8bd383fc50c3418c9655a5c
henry <henry@futunn.com>
2017-01-23 10:27:24 +0800

    [add] 建立项目

---
+++ iOS/ZHYRemotePlayer/ZHYRemotePlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZHYRemotePlayer
+//
+//  Created by John Henry on 2017/1/23.
+//  Copyright © 2017年 John Henry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+     
+    }
+
+
+}
+
